3b2808eaef159c4147616f82aa5527341501e655
hu-mj89-lk <jitesh.maheshwari.tu@gmail.com>
2024-05-15 22:18:57 +0530

    added TSYS Round-2 question

---
+++ java/src/main/java/practice/Tsys_R2.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Tsys_R2 {
+
+    /**
+     * Use two threads t1 & t2 to print a sequence of numbers.
+     * t1 - prints only odd numbers
+     * t2 - prints only even numbers
+     * Output : 1,2,3,4,5,6,7......
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
