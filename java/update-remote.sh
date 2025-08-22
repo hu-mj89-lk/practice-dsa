a25bb68b8169a2f2381591d60af84460634e07c5
hu-mj89-lk <jitesh.maheshwari.tu@gmail.com>
2025-08-23 01:52:38 +0530

    updated: script

--- java/update-remote.sh
+++ java/update-remote.sh
@@ -19,21 +19,33 @@ create_current_message(){
 }
 
 # shellcheck disable=SC3043,SC2046,SC2155
+handle_wip_scenario(){
+  # stash current
+  git stash push -q
+  # stash wip
+  local PREVIOUS_COMMIT_ID=$(git rev-parse HEAD~1)
+  git reset -q --soft "$PREVIOUS_COMMIT_ID"
+  git stash push -q
+  # commit current
+  create_commit 1 "$1"
+  # commit wip
+  create_commit 0 "$2"
+}
+
+handle_normal_scenario(){
+    git add .
+    git commit -q -m "$1"
+}
+
+# shellcheck disable=SC3043,SC2046,SC2155
 main(){
   local CURRENT_COMMIT_MSG=$(create_current_message)
 
   local LATEST_COMMIT_MSG=$(git log -1 --pretty=%B)
   if echo "$LATEST_COMMIT_MSG" | grep -qi "wip"; then
-    # stash current
-    git stash push -q
-    # stash wip
-    local PREVIOUS_COMMIT_ID=$(git rev-parse HEAD~1)
-    git reset -q --soft "$PREVIOUS_COMMIT_ID"
-    git stash push -q
-    # commit current
-    create_commit 1 "$CURRENT_COMMIT_MSG"
-    # commit wip
-    create_commit 0 "$LATEST_COMMIT_MSG"
+    handle_wip_scenario "$CURRENT_COMMIT_MSG" "$LATEST_COMMIT_MSG"
+  else
+    handle_normal_scenario "$CURRENT_COMMIT_MSG"
   fi
 
   git push -q -f
